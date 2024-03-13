9a33a3a7fc9e86c17575efe0537fcf66916e8203
vjroll-180 <vjroll180@gmail.com>
2024-03-13 13:27:00 +0000

    Added basic JS
    Also added typewriter plug in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+function generatePoem(event) {
+  event.preventDefault();
+
+  console.log("Generating....");
+
+  //let poemElement = document.querySelector("#poem");
+  //poemElement.innerHTML = "La tombe dit à la rose";
+  //use type writer instead
+
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let formElement = document.querySelector("#form");
+formElement.addEventListener("submit", generatePoem);
